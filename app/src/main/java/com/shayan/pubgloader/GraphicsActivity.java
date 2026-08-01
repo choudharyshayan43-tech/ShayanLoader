f41711c80ec9b0340c95768d4f57276d5421699c
Shayan <choudharyshayan43-tech@users.noreply.github.com>
2026-08-01 12:03:12 +0500

    Fix guide activity classes

--- app/src/main/java/com/shayan/pubgloader/GraphicsActivity.java
+++ app/src/main/java/com/shayan/pubgloader/GraphicsActivity.java
@@ -3,11 +3,11 @@ package com.shayan.pubgloader;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class SensitivityActivity extends Activity {
+public class GraphicsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sensitivity);
+        setContentView(R.layout.activity_graphics);
     }
 }
